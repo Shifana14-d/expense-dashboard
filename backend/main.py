84457bba855832af6cf9778b7dc7f1e4eae78d76
Shifana Parveen R <shifana.rasheed14@gmail.com>
2026-08-27 00:37:19 +0530

    Update expense dashboard backend

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
+from .database import engine, Base
+from . import models
 
-app= FastAPI(title="Expense Dashboard API")
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Expense Dashboard API")
+
 
 @app.get("/")
 def home():
-    return {
-        "message": "Expense Dashboard API is running!"    
-    }
+    return {"message": "Expense Dashboard API is running"}
 
 
 @app.get("/health")
 def health_check():
-    return{
-        "status": "Healthy"
-    }
-    
+    return {"status": "ok"}
